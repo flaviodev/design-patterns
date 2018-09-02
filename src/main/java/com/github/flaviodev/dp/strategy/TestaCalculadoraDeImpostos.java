1c53e23be9f5e6bf41d2e21493e44d094770a7ed
flavio <fdsdev@gmail.com>
2018-09-02 16:20:38 -0300

    initial commit

---
+++ src/main/java/com/github/flaviodev/dp/strategy/TestaCalculadoraDeImpostos.java
@@ -0,0 +1,19 @@
+package com.github.flaviodev.dp.strategy;
+
+import com.github.flaviodev.dp.builder.OrcamentoBuilder;
+import com.github.flaviodev.dp.model.ItemOrcamento;
+import com.github.flaviodev.dp.model.Orcamento;
+import com.github.flaviodev.dp.model.impostos.ICMS;
+import com.github.flaviodev.dp.model.impostos.ISS;
+
+public class TestaCalculadoraDeImpostos {
+
+	public static void main(String[] args) {
+		Orcamento orcamento = new OrcamentoBuilder().paraCliente("cliente").doVendedor("vendedor")
+				.adicionaItem(new ItemOrcamento("notebook").setQuantidade(1).setValor(1000)).constroi();
+		CalculadorDeImposto calculadora = new CalculadorDeImposto();
+
+		calculadora.realizaCalculo(orcamento, new ISS());
+		calculadora.realizaCalculo(orcamento, new ICMS());
+	}
+}
---
+++ src/test/java/com/github/flaviodev/test/OrcamentoBuilderTest.java
@@ -0,0 +1,30 @@
+package com.github.flaviodev.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import com.github.flaviodev.dp.builder.OrcamentoBuilder;
+import com.github.flaviodev.dp.model.ItemOrcamento;
+import com.github.flaviodev.dp.model.Orcamento;
+
+public class OrcamentoBuilderTest {
+
+	private Logger logger = Logger.getLogger(OrcamentoBuilderTest.class);
+
+	@Test
+	public void deveCriarOrcamento() {
+
+		Orcamento orcamento = new OrcamentoBuilder().paraCliente("José de Oliveira").doVendedor("Anselmo da Cruz")
+				.adicionaItem(new ItemOrcamento("Notebook")).constroi();
+
+		assertNotNull("Orcamento não deve ser null", orcamento);
+		assertEquals("José de Oliveira", orcamento.getNomeCliente());
+		assertEquals("Anselmo da Cruz", orcamento.getNomeVendedor());
+		
+		
+	}
+
+}
